3daeddebe04ad4c4164817c06edf6029a2cd1b69
Michele <michele.cippone01@gmail.com>
2025-01-12 18:48:36 +0100

    PWA beforeinstallprompt implemented

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -7,3 +7,46 @@ if ('serviceWorker' in navigator) {
             console.log('Service Worker registration failed:', error);
         });
 }
+
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent the mini-infobar from appearing on mobile
+    e.preventDefault();
+    e.prompt();
+
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+
+    // Update UI notify the user they can install the PWA
+    showInstallPromotion();
+});
+
+const installButton = document.getElementById('installButton');
+installButton.addEventListener('click', (e) => {
+
+    // Hide the app provided install promotion
+    hideInstallPromotion();
+
+    // Show the install prompt
+    deferredPrompt.prompt();
+
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the install prompt');
+        } else {
+            console.log('User dismissed the install prompt');
+        }
+        deferredPrompt = null;
+    });
+});
+
+function showInstallPromotion() {
+    installButton.style.display = 'block';
+}
+
+function hideInstallPromotion() {
+    installButton.style.display = 'none';
+}
