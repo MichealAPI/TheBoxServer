62324ee66fd51cf2d74bca2e923d3732ca27d2cb
Michele <michele.cippone01@gmail.com>
2025-01-12 17:34:28 +0100

    Fixing PWA scope

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/js/service-worker.js')
+    navigator.serviceWorker.register('/js/service-worker.js', {scope: '/'})
         .then((registration) => {
             console.log('Service Worker registered with scope:', registration.scope);
         })
